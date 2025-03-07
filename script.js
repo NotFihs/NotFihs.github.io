ac05642f0f16c2ef14ef9f011d5d64748a7ee13c
notfihs <113726676+NotFihs@users.noreply.github.com>
2025-03-07 16:31:29 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 <script>
-    setTimeout(() => {
-        window.location.href = "https://www.youtube.com/";
-    }, 2000); // 2000ms = 2 seconds
+    window.location.href = "https://www.youtube.com/";
 </script>
